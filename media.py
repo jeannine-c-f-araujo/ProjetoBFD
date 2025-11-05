b2259300abb061d5075b22d68b1d4c0931e82072
Jeannine <jeannine.freire@hotmail.com>
2025-11-05 11:14:13 -0300

    Adicionamos verificação de aprovação

--- media.py
+++ media.py
@@ -6,7 +6,8 @@ media= (nota1 + nota2)/2
 
 if media >= 7:
     estado ="Aprovado" 
-elif media >= 5:
+elif media >= 4:
     estado = "Recuperação"
 else:
     estado = "Reprovado"
+print(f"Olá {nome}, sua média é : ", média)
