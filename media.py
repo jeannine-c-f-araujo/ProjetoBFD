41b8cf6be4e9d9d1353774cc4d485a9d87f93faa
Jeannine <jeannine.freire@hotmail.com>
2025-11-05 10:27:36 -0300

    Inicio do código de média

---
+++ media.py
@@ -0,0 +1,7 @@
+nome= input("Digite o nome do aluno: ")
+nota1 = float(input("Digite a 1 nota: "))
+nota2 = float(input("digite a 2 nota: "))
+
+media= (nota1 + nota2)/2
+print(media)
+print(f"Olá", nome, "sua média é:", media)
